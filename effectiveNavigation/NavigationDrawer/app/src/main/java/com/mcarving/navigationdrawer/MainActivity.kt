5c79f248fafca1ab6ea8d6c1e013c9e3554b4bd3
Eric-Cen <scen01@gmail.com>
2018-12-19 11:47:46 -0500

    feat: navigation drawer

---
+++ effectiveNavigation/NavigationDrawer/app/src/main/java/com/mcarving/navigationdrawer/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.mcarving.navigationdrawer
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBar
+import android.support.v7.widget.Toolbar
+import android.view.MenuItem
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mDrawerLayout: DrawerLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        val actionbar : ActionBar? = supportActionBar
+        actionbar?.apply{
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.ic_menu)
+        }
+
+        mDrawerLayout = findViewById(R.id.drawer_layout)
+
+        mDrawerLayout.addDrawerListener(
+            object : DrawerLayout.DrawerListener {
+                override fun onDrawerSlide(drawerView: View, slideOffset: Float) {
+                    // Respond when the drawer's position changes
+                }
+
+                override fun onDrawerOpened(drawerView: View) {
+                    // Respond when the drawer is opened
+                }
+
+                override fun onDrawerClosed(drawerView: View) {
+                    // Respond when the drawer is closed
+                }
+
+                override fun onDrawerStateChanged(newState: Int) {
+                    // Respond when the drawer motion state changes
+                }
+            }
+        )
+
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            // set item as selected to persist highlight
+            menuItem.isChecked = true
+            // close drawer when item is tapped
+            mDrawerLayout.closeDrawers()
+
+            // Add code here to update the UI based on the item selected
+            // for example, swap UI fragments here
+            true
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when(item?.itemId){
+            android.R.id.home -> {
+                mDrawerLayout.openDrawer(GravityCompat.START)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
